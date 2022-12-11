5a98dfbace96649e60eb1040d7247498e81ce77e
Felix Krause <felix.krause@bit.admin.ch>
2022-12-11 20:42:59 +0100

    add basic quiz

--- ui/src/app/quiz/quiz.component.ts
+++ ui/src/app/quiz/quiz.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CardService } from '../services/card.service';
+import { DeckService } from '../services/deck.service';
+import { Card } from '../_dto/Card';
+import { Deck } from '../_dto/Deck';
 
 @Component({
   selector: 'app-quiz',
@@ -6,19 +11,63 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quiz.component.scss'],
 })
 export class QuizComponent implements OnInit {
+  get deck_id(): number {
+    return parseInt(this.activatedRoute.snapshot.paramMap.get('deck_id') ?? '');
+  }
   state: 'start' | 'quiz' | 'end' = 'start';
+  flipped = true;
+  deck?: Deck;
+  cards?: Card[];
+  currentCard?: Card;
 
-  constructor() {}
+  quizData: any[] = [];
 
-  ngOnInit(): void {}
+  constructor(
+    private readonly cardService: CardService,
+    private readonly deckService: DeckService,
+    private readonly activatedRoute: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    this.deckService.get(this.deck_id).subscribe((deck) => {
+      this.deck = deck;
+    });
+
+    this.cardService.getAll(this.deck_id).subscribe((cards) => {
+      this.cards = cards;
+    });
+  }
 
   start() {
     this.state = 'quiz';
+    this.nextCard(false);
+    // TODO: start timer
   }
-  quiz() {
+  stop() {
+    // TODO: Stop the timer
     this.state = 'end';
   }
-  end() {
-    this.state = 'start';
+  flipCard() {
+    this.flipped = true;
+    // TODO: pause timer
+  }
+  nextCard(answer: boolean) {
+    if (this.currentCard) {
+      this.quizData.push({
+        cardId: this.currentCard.id,
+        time: 0,
+        answer,
+      });
+    }
+
+    if (this.cards && this.cards.length > 0) {
+      const id = Math.floor(Math.random() * this.cards.length);
+
+      this.currentCard = this.cards.splice(id, 1)[0];
+      this.flipped = false;
+      // TODO: restart time
+    } else {
+      this.stop();
+    }
   }
 }
